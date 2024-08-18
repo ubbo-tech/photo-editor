03432508a17e231a83078a673149f2c5ea0496e1
berkay <berkay.baskaya@ginitalent.com>
2024-08-19 00:02:26 +0300

    version log added on app initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cropyai';
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log("verison 19 Aug")
+  }
 }
